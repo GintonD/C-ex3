9ae0d11ebb248caa132477f9dbcc56b23363ab53
GintonD <53877729+GintonD@users.noreply.github.com>
2019-12-20 01:09:44 +0200

    more fixes

--- isort.c
+++ isort.c
@@ -1,30 +1,40 @@
-#define size_arr 5
+#define size_arr 50
 #include <stdio.h>
+
 void printArray(int *arr, int n) 
 { 
     int i; 
-    for (i = 0; i < n-1; i++) {
+    for (i = 0; i < n-1; i++)
+    {
         printf("%d ,", *(arr+i)); 
     }// end for
+
 printf("%d", *(arr+i));
 printf("\n");
 } 
-void shift_element (int *arr, int i){
-for (int j=i-1; j>0; j--){
-         *( arr+1+j)=*(arr+j);
-} //end for
-} //end function
-void insertionSort(int *arr, int length) 
+
+//***************************************************
+void shift_element(int *arr, int i)
+{
+for (int j=i-1; j>0; j--)
+	*( arr+1+j)=*(arr+j);
+ //end for
+}
+//**************************************************
+void insertion_sort(int *arr, int len)
 { 
     int val,i , j; 
-    for (i = 1; i < length; i++) { 
+    for (i = 1; i < len; i++)
+    {
        val= *(arr+i);
         j = i - 1; 
-        int counter=0;
+       // int counter=0;
         //here we need to chenge with shift element
-        while ( (*(arr+j) > val) && j>=0) {
-               arr[j + 1] = arr[j];  
-            j = j - 1;          }   
+        while ( (*(arr+j) > val) && j>=0)
+        {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
          
          
             // until here
@@ -32,24 +42,35 @@ void insertionSort(int *arr, int length)
        *(arr +j+1) = val; 
     } //end for
  }//end functionn
+
+//**********************************************
   int main() 
-{ 
+  {
     int arr[size_arr];
-    printf ("enter 50 number into array \n");
-    for (int i=0; i<size_arr; i++){
-     printf ("plese insert number in arr. now in place:  %d \n",i+1);
-    scanf("%d", &*(arr+i));   
-    }
+    int a;
+
+    //input
+    printf ("enter 50 number into array: \n");
+
+    for(int i=0 ; i<size_arr; i++)
+    {
+    	scanf("%d", &a);
+    	*(arr+i)=a;
+  	}
+
+    //output
     printf("Before Arr \n");
     printArray(arr, size_arr); 
     
-    // shift_element(arr, size_arr); 
-    // printf("After shift_element \n");
-    // printArray(arr, size_arr); 
-    // printf("After insetionSort \n");
-    insertionSort(arr, size_arr);
-    printArray(arr, size_arr); 
+    insertion_sort(arr, size_arr);
 
+    printf("After insetionSort \n");
+    printArray(arr, size_arr);
 
-}
+     //shift_element(arr, size_arr);
+     //printf("After shift_element \n");
+     //printArray(arr, size_arr);
+
+return 0;
+  }
 
