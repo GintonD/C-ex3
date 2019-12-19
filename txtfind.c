d04f759202c9b7e26e0918fa4a0c918cba1fb576
GintonD <53877729+GintonD@users.noreply.github.com>
2019-12-19 22:07:05 +0200

    fix bugs

--- txtfind.c
+++ txtfind.c
@@ -2,96 +2,78 @@
  * txtfind.c
  *
  *
+ * Aouters: Ginton & fooxi
+ *
+ *
  */
 #include <stdio.h>
 #include <string.h>
 #define LINE 256
 #define WORD 30
+
+
 // This function need to check if str contain word
 
 int substring (char *str, char *word)
 {
-          int i=0;
+  int i=0;
 
-       while (*str!='\0')
-       {
-       if (*str == *(word+i)){ //check if the char  is equals
-          i++;
-          if (strlen(word)==i) return 1; //return true
-       }
-       else
-       {
-                 i=0;
-       }
+  while (*str!='\0')
+  {
+     if (*str == *(word+i))
+     { //check if the char  is equals
+        i++;
+        if (strlen(word)==i)
+        	return 1; //return true
+     }
+     else
+    	 i=0;
 
-       str++;
-       }
+     str++;
+   }
 return 0; // return false
 
 }
 
+
+
+
 // This functin check if the word similiar until num of change to str
-int similar (char *str, char *word, int change)
+int similar (char *s, char *t, int n)
 {
-          int i=0;
-          int j=-1; // num of mistake
-       while (*str!='\0')
-       {
-       if (*str == *(word+i)){
-          i++;
-          if (strlen(word)==i) return 1;
-       }
-       else // if the char is not equals
-       {
-                 j++; // num of mistake
-                 if (j==change){
-                    i=0;
-                    j=0;
-                 }
+	int i=0;
+    int j=-1;// num of mistake
 
-       }
+    if (strlen(s)>strlen(t)+n)
+    	return 0;
 
-       str++; // pointers ++
-       }
+    while (*s!='\0')
+    {
+    	if (*s == *(t+i))
+    	{
+    		i++;
+    		if (strlen(t)==i)
+    			return 1;
+    	}
+
+    	else // if the char is not equals
+    	{
+    		j++; // num of mistake
+            if (j==n)
+              {
+                 i=0;
+                 j=0;
+              }
+
+    	 }
+
+       s++; // pointers ++
+     }
 
 return 0;
-
 }
 
-
-// this function need to split word from str
-//have a problem with function
-int word_split( char *str, char *word, int counter) //for foxi: i dont use this function. do you?
-{
-//           printf("%d""\n", sub);
-
-          int i=0;
-           while ((*str!=' '))
-          {
-          *(word)= *(str+counter);
-          word++;
-          str++;
-          i++;
-          }
-          *word='\0';
-          return i+1;
-}
-//this function need to split lines
-
-int line_split( char *str, char *word, int counter) //for foxi: i dont use this function. do you?
-{
-
-          int i=0;
-           while ((*str!='\0')) //need to change to \n or '\0'
-          {
-          *(word)= *(str+counter);
-          word++;
-          str++;
-          i++;
-          }
-          *word='\0';
-          return i+1;
-}
+//*************************************
 
 int getLine(char s[])
 {
@@ -116,7 +98,7 @@ int getLine(char s[])
 
 }
 
-
+//*********************************************************
 
 int getword(char w[])
 {
@@ -127,15 +109,19 @@ int getword(char w[])
 	{
 		w[SizeWord]=letter;
 		SizeWord++;
-
 		letter = getchar();
 	}
+
+
+
 	w[SizeWord]='\0'; // end of the srtring;
 
 
 	return SizeWord;
 
 }
+
+//*****************************************************
 
 
 void print_lines(char * str)
@@ -153,22 +139,28 @@ void print_lines(char * str)
 
 }
 
-
+//*****************************************************
 
 void print_similar_words(char * str)
 {
 	char word[WORD];
 	int SizeWord = getword(word);
+
+
 	while(SizeWord != 0)
 	{
+
 		if(similar(word, str,1)==1)
 			printf("%s\n", word); //check it
 
-		//SizeWord = getLine(word); //word.next();
+
 		SizeWord = getword(word); //word.next();
 	}
 
+
 }
+
+//******************************************************
 
 int main ()
 {
@@ -182,7 +174,8 @@ int main ()
 
 
    scanf(" %c\n" , &option);
-   //printf("op= %c\n" , option);
+
+
 
 
    //Part B
@@ -194,10 +187,5 @@ int main ()
    		print_similar_words(word);
 
 
-   	return 0;
-
-   }
-
-
-
+}
 
